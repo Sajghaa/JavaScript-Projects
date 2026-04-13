1cc8a114269d5999b654753492f86b468c35380e
Serge <sajghaa@gmail.com>
2026-04-13 17:51:32 +0200

    Updaed EditorManager.js

--- 77-portfolio-generator-self-deploy/js/modules/EditorManager.js
+++ 77-portfolio-generator-self-deploy/js/modules/EditorManager.js
@@ -1,62 +1,12 @@
-export class EditorManager {
+class EditorManager {
     constructor(stateManager, eventBus) {
         this.stateManager = stateManager;
         this.eventBus = eventBus;
-        
-        this.initializeInputs();
-    }
-
-    initializeInputs() {
-        const personalFields = [
-            'userName', 'userTitle', 'userEmail', 'userPhone',
-            'userLocation', 'userBio', 'userAvatar', 'githubUrl',
-            'linkedinUrl', 'twitterUrl', 'codepenUrl'
-        ];
-
-        personalFields.forEach(field => {
-            const element = document.getElementById(field);
-            if (element) {
-                const statePath = this.getStatePath(field);
-                element.value = this.stateManager.get(statePath) || '';
-                
-                element.addEventListener('input', (e) => {
-                    this.stateManager.set(statePath, e.target.value);
-                    this.eventBus.emit('state:changed');
-                });
-            }
-        });
-    }
-
-    loadPersonalInfo() {
-        const personal = this.stateManager.get('personal');
-        
-        document.getElementById('userName').value = personal.name || '';
-        document.getElementById('userTitle').value = personal.title || '';
-        document.getElementById('userEmail').value = personal.email || '';
-        document.getElementById('userPhone').value = personal.phone || '';
-        document.getElementById('userLocation').value = personal.location || '';
-        document.getElementById('userBio').value = personal.bio || '';
-        document.getElementById('userAvatar').value = personal.avatar || '';
-        document.getElementById('githubUrl').value = personal.github || '';
-        document.getElementById('linkedinUrl').value = personal.linkedin || '';
-        document.getElementById('twitterUrl').value = personal.twitter || '';
-        document.getElementById('codepenUrl').value = personal.codepen || '';
-    }
-
-    getStatePath(fieldId) {
-        const mapping = {
-            userName: 'personal.name',
-            userTitle: 'personal.title',
-            userEmail: 'personal.email',
-            userPhone: 'personal.phone',
-            userLocation: 'personal.location',
-            userBio: 'personal.bio',
-            userAvatar: 'personal.avatar',
-            githubUrl: 'personal.github',
-            linkedinUrl: 'personal.linkedin',
-            twitterUrl: 'personal.twitter',
-            codepenUrl: 'personal.codepen'
-        };
-        return mapping[fieldId];
+        this.editorPanel = new EditorPanel(stateManager, eventBus);
+        this.projectEditor = new ProjectEditor(stateManager, eventBus);
+        this.skillEditor = new SkillEditor(stateManager, eventBus);
+        this.themeSelector = new ThemeSelector(stateManager, eventBus);
     }
 }
+
+window.EditorManager = EditorManager;
