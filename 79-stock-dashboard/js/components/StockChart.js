03e44c2b1c2f38a6227ae3eb9797ae15b74b5f40
Serge <sajghaa@gmail.com>
2026-04-19 16:56:29 +0200

    Updated  js/components/StockChart.js

---
+++ 79-stock-dashboard/js/components/StockChart.js
@@ -0,0 +1,9 @@
+// StockChart.js - Manages stock chart display
+class StockChart {
+    constructor(stateManager, eventBus) {
+        this.stateManager = stateManager;
+        this.eventBus = eventBus;
+    }
+}
+
+window.StockChart = StockChart;
