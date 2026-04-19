d871f06a2419c56e9d4c4e285d02288ca0a6cf8e
Serge <sajghaa@gmail.com>
2026-04-19 16:56:45 +0200

    Updated  js/components/PortfolioWidget.js

---
+++ 79-stock-dashboard/js/components/PortfolioWidget.js
@@ -0,0 +1,9 @@
+// PortfolioWidget.js - Portfolio display widget
+class PortfolioWidget {
+    constructor(stateManager, eventBus) {
+        this.stateManager = stateManager;
+        this.eventBus = eventBus;
+    }
+}
+
+window.PortfolioWidget = PortfolioWidget;
