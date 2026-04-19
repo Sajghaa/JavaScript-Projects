621e99b0dd2b7f7ed620eefc322e0a9f83ba407a
Serge <sajghaa@gmail.com>
2026-04-19 16:53:40 +0200

    updated js/components/AlertModal.js

---
+++ 79-stock-dashboard/js/components/AlertModal.js
@@ -0,0 +1,9 @@
+// AlertModal.js - Manages alert modal
+class AlertModal {
+    constructor(stateManager, eventBus) {
+        this.stateManager = stateManager;
+        this.eventBus = eventBus;
+    }
+}
+
+window.AlertModal = AlertModal;
