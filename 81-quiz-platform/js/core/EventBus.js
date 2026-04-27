e85bb75cdad09cc522a9e51682e0e73a3fdeb8c8
Serge <sajghaa@gmail.com>
2026-04-27 22:59:50 +0200

    Updated EventBus.js

--- 81-quiz-platform/js/core/EventBus.js
+++ 81-quiz-platform/js/core/EventBus.js
@@ -1,33 +1,14 @@
 class EventBus {
-    constructor() {
-        this.events = new Map();
-    }
-
+    constructor() { this.events = new Map(); }
     on(event, callback) {
-        if (!this.events.has(event)) {
-            this.events.set(event, new Set());
-        }
+        if (!this.events.has(event)) this.events.set(event, new Set());
         this.events.get(event).add(callback);
-        return () => this.off(event, callback);
+        return () => this.events.get(event)?.delete(callback);
     }
-
-    off(event, callback) {
-        if (this.events.has(event)) {
-            this.events.get(event).delete(callback);
-        }
-    }
-
     emit(event, data) {
         if (this.events.has(event)) {
-            this.events.get(event).forEach(callback => {
-                try {
-                    callback(data);
-                } catch (error) {
-                    console.error(`Error in event ${event}:`, error);
-                }
-            });
+            this.events.get(event).forEach(callback => { try { callback(data); } catch(e) { console.error(e); } });
         }
     }
 }
-
 window.EventBus = EventBus;
