04c62627fa399f89e085b14dcca4dd9ec52532e4
Serge <sajghaa@gmail.com>
2026-04-29 17:52:51 +0200

    Updated EventBus.js

---
+++ 83-kanban-multi-user/js/core/EventBus.js
@@ -0,0 +1,21 @@
+class EventBus {
+    constructor() {
+        this.events = new Map();
+    }
+
+    on(event, callback) {
+        if (!this.events.has(event)) this.events.set(event, new Set());
+        this.events.get(event).add(callback);
+        return () => this.events.get(event)?.delete(callback);
+    }
+
+    emit(event, data) {
+        if (this.events.has(event)) {
+            this.events.get(event).forEach(callback => {
+                try { callback(data); } catch (e) { console.error(e); }
+            });
+        }
+    }
+}
+
+window.EventBus = EventBus;
