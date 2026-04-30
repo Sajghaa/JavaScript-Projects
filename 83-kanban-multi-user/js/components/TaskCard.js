e47defc5e022daae004048ee507ab3e0eb76231a
Serge <sajghaa@gmail.com>
2026-04-30 22:12:14 +0200

    Updated js/components/TaskCard.js

--- 83-kanban-multi-user/js/components/TaskCard.js
+++ 83-kanban-multi-user/js/components/TaskCard.js
@@ -2,6 +2,7 @@ class TaskCard {
     constructor(stateManager, eventBus) {
         this.stateManager = stateManager;
         this.eventBus = eventBus;
+        this.userAvatar = new UserAvatar(stateManager, eventBus);
     }
 
     render(task) {
@@ -16,28 +17,15 @@ class TaskCard {
                     <div class="task-title">${this.escapeHtml(task.title)}</div>
                     <span class="task-priority priority-${task.priority}">${task.priority}</span>
                 </div>
-                
-                ${task.description ? `
-                    <div class="task-description">${this.escapeHtml(task.description.substring(0, 80))}${task.description.length > 80 ? '...' : ''}</div>
-                ` : ''}
-                
-                ${task.tags?.length ? `
-                    <div class="task-tags">
-                        ${task.tags.map(tag => `<span class="task-tag">${this.escapeHtml(tag)}</span>`).join('')}
-                    </div>
-                ` : ''}
-                
+                ${task.description ? `<div class="task-description">${this.escapeHtml(task.description.substring(0, 80))}${task.description.length > 80 ? '...' : ''}</div>` : ''}
+                ${task.tags?.length ? `<div class="task-tags">${task.tags.map(tag => `<span class="task-tag">${this.escapeHtml(tag)}</span>`).join('')}</div>` : ''}
                 <div class="task-meta">
                     <div class="task-due-date ${isOverdue ? 'overdue' : ''}">
-                        <i class="far fa-calendar"></i>
-                        ${dueDate ? dueDate.toLocaleDateString() : 'No due date'}
+                        <i class="far fa-calendar"></i> 
+                        ${dueDate ? dueDate.toLocaleDateString() : 'No date'}
                     </div>
                     <div class="task-assignees">
-                        ${assignees.map(user => `
-                            <div class="assignee-avatar" style="background: ${user.color}" title="${user.name}">
-                                ${user.avatar}
-                            </div>
-                        `).join('')}
+                        ${assignees.map(user => this.userAvatar.renderAssignee(user)).join('')}
                     </div>
                 </div>
             </div>
