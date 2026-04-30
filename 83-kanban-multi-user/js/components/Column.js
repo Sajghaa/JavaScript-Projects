66d0f013045d237c8f48ec8e23a1e56aee62a815
Serge <sajghaa@gmail.com>
2026-04-30 22:11:59 +0200

    Updated js/components/Column.js

--- 83-kanban-multi-user/js/components/Column.js
+++ 83-kanban-multi-user/js/components/Column.js
@@ -9,35 +9,14 @@ class Column {
         return `
             <div class="column" data-column-id="${column.id}">
                 <div class="column-header">
-                    <div class="column-title">
-                        <span>${column.icon}</span>
-                        <h3>${column.title}</h3>
-                        <span class="column-badge">${tasks.length}</span>
-                    </div>
-                    <div class="column-actions">
-                        <button class="column-action add-task-btn" title="Add task">
-                            <i class="fas fa-plus"></i>
-                        </button>
-                        <button class="column-action" title="More options">
-                            <i class="fas fa-ellipsis-h"></i>
-                        </button>
-                    </div>
+                    <div class="column-title"><span>${column.icon}</span><h3>${column.title}</h3><span class="column-badge">${tasks.length}</span></div>
+                    <button class="column-action add-task-btn" title="Add task"><i class="fas fa-plus"></i></button>
                 </div>
-                <div class="tasks-container" data-column-id="${column.id}">
-                    ${tasks.map(task => this.taskCard.render(task)).join('')}
-                </div>
+                <div class="tasks-container" data-column-id="${column.id}">${tasks.map(task => this.taskCard.render(task)).join('')}</div>
             </div>
         `;
     }
 
-    attachEvents(element, columnId) {
-        const addBtn = element.querySelector('.add-task-btn');
-        if (addBtn) {
-            addBtn.onclick = () => {
-                this.eventBus.emit('task:add', columnId);
-            };
-        }
-    }
+    attachEvents(element, columnId) { element.querySelector('.add-task-btn').onclick = () => this.eventBus.emit('task:add', columnId); }
 }
-
 window.Column = Column;
