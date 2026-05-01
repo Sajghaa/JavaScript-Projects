665c11c41c01e2dff28c39802d0e3baf004f2f3f
Serge <sajghaa@gmail.com>
2026-05-01 21:19:52 +0200

    js/modules/TaskManager.js

--- 83-kanban-multi-user/js/modules/TaskManager.js
+++ 83-kanban-multi-user/js/modules/TaskManager.js
@@ -3,87 +3,37 @@ class TaskManager {
         this.stateManager = stateManager;
         this.eventBus = eventBus;
         this.taskModal = taskModal;
-        this.taskCard = new TaskCard(stateManager, eventBus);
         this.init();
     }
 
     init() {
-        this.eventBus.on('task:add', (columnId) => this.taskModal.showAddTask(columnId));
-        this.eventBus.on('task:edit', (taskId) => this.taskModal.showEditTask(taskId));
-        this.eventBus.on('task:create', (taskData) => this.createTask(taskData));
-        this.eventBus.on('task:update', ({ taskId, updates }) => this.updateTask(taskId, updates));
-        this.eventBus.on('task:delete', (taskId) => this.deleteTask(taskId));
+        this.eventBus.on('task:add', (colId) => this.taskModal.showAddTask(colId));
+        this.eventBus.on('task:edit', (id) => this.taskModal.showEditTask(id));
+        this.eventBus.on('task:create', (data) => this.createTask(data));
+        this.eventBus.on('task:update', ({taskId, updates}) => this.updateTask(taskId, updates));
+        this.eventBus.on('task:delete', (id) => this.deleteTask(id));
+        document.getElementById('addTaskBtn').onclick = () => this.eventBus.emit('task:add', 'todo');
     }
 
-    createTask(taskData) {
-        const currentUser = this.stateManager.getCurrentUser();
-        const newTask = this.stateManager.addTask(taskData);
-        
-        this.eventBus.emit('toast', { message: 'Task created successfully!', type: 'success' });
+    createTask(data) {
+        const newTask = this.stateManager.addTask(data);
+        this.eventBus.emit('toast', { message: 'Task created!', type: 'success' });
         this.eventBus.emit('board:refresh');
-        
-        this.stateManager.addActivity({
-            userId: currentUser.id,
-            userName: currentUser.name,
-            type: 'task_created',
-            taskId: newTask.id,
-            message: `${currentUser.name} created task "${newTask.title}"`
-        });
     }
 
-    updateTask(taskId, updates) {
-        const currentUser = this.stateManager.getCurrentUser();
-        const result = this.stateManager.updateTask(taskId, updates);
-        
-        if (result) {
-            this.eventBus.emit('toast', { message: 'Task updated successfully!', type: 'success' });
-            this.eventBus.emit('board:refresh');
-            
-            this.stateManager.addActivity({
-                userId: currentUser.id,
-                userName: currentUser.name,
-                type: 'task_updated',
-                taskId: taskId,
-                message: `${currentUser.name} updated task "${result.new.title}"`
-            });
-        }
+    updateTask(id, updates) {
+        this.stateManager.updateTask(id, updates);
+        this.eventBus.emit('toast', { message: 'Task updated!', type: 'success' });
+        this.eventBus.emit('board:refresh');
     }
 
-    deleteTask(taskId) {
-        const currentUser = this.stateManager.getCurrentUser();
-        const task = this.stateManager.deleteTask(taskId);
-        
-        if (task) {
-            this.eventBus.emit('toast', { message: 'Task deleted!', type: 'info' });
-            this.eventBus.emit('board:refresh');
-            
-            this.stateManager.addActivity({
-                userId: currentUser.id,
-                userName: currentUser.name,
-                type: 'task_deleted',
-                taskId: taskId,
-                message: `${currentUser.name} deleted task "${task.title}"`
-            });
-        }
+    deleteTask(id) {
+        if(confirm('Delete this task?')) { this.stateManager.deleteTask(id); this.eventBus.emit('toast', { message: 'Task deleted', type: 'info' }); this.eventBus.emit('board:refresh'); }
     }
 
-    moveTask(taskId, newColumnId) {
-        const currentUser = this.stateManager.getCurrentUser();
-        const task = this.stateManager.moveTask(taskId, newColumnId);
-        const column = this.stateManager.get('columns').find(c => c.id === newColumnId);
-        
-        if (task) {
-            this.eventBus.emit('board:refresh');
-            
-            this.stateManager.addActivity({
-                userId: currentUser.id,
-                userName: currentUser.name,
-                type: 'task_moved',
-                taskId: taskId,
-                message: `${currentUser.name} moved task "${task.title}" to ${column?.title || newColumnId}`
-            });
-        }
+    moveTask(id, newColId) {
+        this.stateManager.updateTask(id, { columnId: newColId });
+        this.eventBus.emit('board:refresh');
     }
 }
-
 window.TaskManager = TaskManager;
