c1c485e4eb1c685f4f0c00a0f439423bd830c37f
Serge <sajghaa@gmail.com>
2026-05-06 21:40:00 +0200

    Updated GameOverlay.js

---
+++ 90-tetris-game/js/components/GameOverlay.js
@@ -0,0 +1,29 @@
+class GameOverlay {
+    constructor(stateManager, eventBus) {
+        this.stateManager = stateManager;
+        this.eventBus = eventBus;
+        this.init();
+    }
+
+    init() {
+        this.eventBus.on('game:over', () => this.showGameOver());
+        this.eventBus.on('game:paused', () => this.showPause());
+        this.eventBus.on('game:resumed', () => this.hidePause());
+        this.eventBus.on('game:reset', () => this.hideAll());
+        
+        document.getElementById('restartBtn').onclick = () => this.eventBus.emit('game:restart');
+        document.getElementById('resumeBtn').onclick = () => this.eventBus.emit('game:togglePause');
+    }
+
+    showGameOver() {
+        document.getElementById('finalScore').textContent = this.stateManager.get('score');
+        document.getElementById('finalLines').textContent = this.stateManager.get('lines');
+        document.getElementById('finalHighScore').textContent = this.stateManager.get('highScore');
+        document.getElementById('gameOverlay').style.display = 'flex';
+    }
+
+    showPause() { document.getElementById('pauseOverlay').style.display = 'flex'; }
+    hidePause() { document.getElementById('pauseOverlay').style.display = 'none'; }
+    hideAll() { document.getElementById('gameOverlay').style.display = 'none'; document.getElementById('pauseOverlay').style.display = 'none'; }
+}
+window.GameOverlay = GameOverlay;
