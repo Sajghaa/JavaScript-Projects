d4d82bccd00f3c13031cc7be6ec81538c21d1d32
Serge <sajghaa@gmail.com>
2026-05-06 21:42:23 +0200

    Updated GameEngine.js

---
+++ 90-tetris-game/js/core/GameEngine.js
@@ -0,0 +1,54 @@
+class GameEngine {
+    constructor(stateManager, eventBus) {
+        this.stateManager = stateManager;
+        this.eventBus = eventBus;
+        this.gameLoop = null;
+        this.isRunning = false;
+        this.isPaused = false;
+        this.speed = 500;
+        this.dropInterval = null;
+    }
+
+    start() {
+        if(this.gameLoop) clearInterval(this.gameLoop);
+        this.isRunning = true;
+        this.isPaused = false;
+        this.gameLoop = setInterval(() => {
+            if(!this.isPaused && this.isRunning) {
+                this.eventBus.emit('game:tick');
+            }
+        }, this.speed);
+    }
+
+    pause() {
+        if(this.isRunning && !this.isPaused) {
+            this.isPaused = true;
+            this.eventBus.emit('game:paused');
+        }
+    }
+
+    resume() {
+        if(this.isRunning && this.isPaused) {
+            this.isPaused = false;
+            this.eventBus.emit('game:resumed');
+        }
+    }
+
+    stop() {
+        if(this.gameLoop) {
+            clearInterval(this.gameLoop);
+            this.gameLoop = null;
+        }
+        this.isRunning = false;
+        this.isPaused = false;
+    }
+
+    setSpeed(speed) {
+        this.speed = speed;
+        if(this.isRunning && !this.isPaused) {
+            this.stop();
+            this.start();
+        }
+    }
+}
+window.GameEngine = GameEngine;
