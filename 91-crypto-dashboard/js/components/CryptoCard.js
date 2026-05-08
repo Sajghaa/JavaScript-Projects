5e89c98c8e8d1d9d671e6c696c55bfe8ea2959d1
Serge <sajghaa@gmail.com>
2026-05-08 15:25:00 +0200

    Updated CryptoCard.js

--- 91-crypto-dashboard/js/components/CryptoCard.js
+++ 91-crypto-dashboard/js/components/CryptoCard.js
@@ -5,11 +5,12 @@ class CryptoCard {
     }
 
     render(coin, isActive = false) {
-        const change = coin.price_change_percentage_24h;
+        const change = coin.price_change_percentage_24h !== null && coin.price_change_percentage_24h !== undefined ? coin.price_change_percentage_24h : 0;
+        const price = coin.current_price !== null && coin.current_price !== undefined ? coin.current_price : 0;
         return `
             <div class="crypto-card ${isActive ? 'active' : ''}" data-id="${coin.id}">
                 <div class="crypto-info">
-                    <div class="crypto-rank">#${coin.market_cap_rank}</div>
+                    <div class="crypto-rank">#${coin.market_cap_rank || 'N/A'}</div>
                     <div class="crypto-icon"><i class="fab fa-${this.getIcon(coin.symbol)}"></i></div>
                     <div>
                         <div class="crypto-name">${coin.name}</div>
@@ -17,7 +18,7 @@ class CryptoCard {
                     </div>
                 </div>
                 <div class="crypto-price">
-                    <div class="crypto-price-value">$${coin.current_price.toLocaleString()}</div>
+                    <div class="crypto-price-value">$${price.toLocaleString()}</div>
                     <div class="crypto-change ${change >= 0 ? 'positive' : 'negative'}">${change >= 0 ? '+' : ''}${change.toFixed(2)}%</div>
                 </div>
             </div>
